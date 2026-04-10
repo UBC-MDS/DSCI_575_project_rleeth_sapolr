3f50f887e22911e58caf59573c11066e4a951def
RANDALL Lee <randallxlee@gmail.com>
2026-04-09 22:01:32 -0700

    Changes to semantic and BM25 models

--- src/bm25.py
+++ src/bm25.py
@@ -1,3 +1,47 @@
 from langchain_community.retrievers import BM25Retriever
+from langchain_core.documents import Document
+from rank_bm25 import BM25Okapi
+import re
 
 def text_tokenizer(text: str) -> list[str]:
+    stopwords = {
+    "the","a","an","and","is","are","to","of","in",
+    "for","on","with","this","that","it","be"
+    }
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    tokens = text.split()
+    tokens = [t for t in tokens if t not in stopwords]
+    return tokens
+
+def build_bm25(documents: list[Document]):
+
+    texts = [doc.page_content for doc in documents]
+
+    tokenized_corpus = [
+        text_tokenizer(text)
+        for text in texts
+    ]
+
+    bm25 = BM25Okapi(tokenized_corpus)
+
+    return bm25
+
+def bm25_search(bm25, documents: list[Document], query: str, k=5):
+
+    tokenized_query = text_tokenizer(query)
+
+    scores = bm25.get_scores(tokenized_query)
+
+    ranked_idx = sorted(
+        range(len(scores)),
+        key=lambda i: scores[i],
+        reverse=True
+    )[:k]
+
+    results = [
+        (documents[i], scores[i])
+        for i in ranked_idx
+    ]
+
+    return results
---
+++ src/semantic.py
@@ -0,0 +1,35 @@
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
+
+def semantic_search(documents, query, k=5, sample_size=None, reload_index=False):
+    
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+    
+    index_path = "../data/processed/faiss_index"
+
+    # Load index if it exists
+    if os.path.exists(index_path) and not reload_index:
+        vectorstore = FAISS.load_local(
+            index_path,
+            embedding_model,
+            allow_dangerous_deserialization=True
+        )
+
+    # Otherwise create and save it
+    else:
+        docs = documents if sample_size is None else documents[:sample_size]
+
+        vectorstore = FAISS.from_documents(
+            docs,
+            embedding_model
+        )
+
+        vectorstore.save_local(index_path)
+
+    results = vectorstore.similarity_search_with_score(query, k=k)
+
+
+    return results
